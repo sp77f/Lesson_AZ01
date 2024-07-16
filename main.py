632b6ea10426d5ac08936c3c560f91f76b0ed0c9
sp77f <spyf77@gmail.com>
2024-07-16 12:43:01 +0300

    add code

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv('data.csv')
+#печать заголовков  о датасете (первые 5 строк)
+print(df.head())
+#печать информации  о датасете
+print(df.info())
+#печать описания  о датасете
+print(df.describe())
+df1 = pd.read_csv('dz.csv')
+#печать средней зарплаты в городах
+group =  df1.groupby('City')['Salary'].mean()
+print(group)
